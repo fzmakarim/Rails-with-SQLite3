8f47d069841291e39809698a7b11c3bf39158168
mahmud fauzi <mahmudfz20@gmail.com>
2022-04-22 13:41:19 +0000

    generate seed for categories table

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,12 @@
 class Menu < ApplicationRecord
+    has_and_belongs_to_many :categories do
+        def get_names
+          
+          map(&:name)
+        end
+    end
+    validates :name, presence: true,  uniqueness: true
+    validates :price, presence: true, numericality: true, comparison: {greater_than_or_equal_to: 0.01 }
+    validates :categories, presence: true
+  
 end
